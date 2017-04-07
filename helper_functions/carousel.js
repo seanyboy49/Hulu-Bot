ab2f2b370dd6f68cb98d4b91bcd01653faebd1d8
Sean Lee <seanboramlee@gmail.com>
2017-04-07 13:57:32 -0700

    test two elements

--- helper_functions/carousel.js
+++ helper_functions/carousel.js
@@ -25,6 +25,20 @@ function sendMovieCarousel(recipientId) {
                   title: "Watch this!"
                 }
               ]
+            },
+            {
+              title: "Rick and Morty",
+              image_url: "",
+              subtitle: "Rick and Morty is a show about a sociopathic scientist.",
+              item_url: "https://www.hulu.com/rick-and-morty",
+              image_url: "https://ib1.hulu.com/show_key_art/22881?size=1600x600&region=US",
+              buttons: [
+                {
+                  type: "web_url",
+                  url: "https://www.hulu.com/rick-and-morty",
+                  title: "Watch this!"
+                }
+              ]
             }
           ]
         }
