421128c0e2bd7d116a8dc899d93c4b9205e69dbc
Sean Lee <seanboramlee@gmail.com>
2017-04-07 13:37:02 -0700

    add rick and morty image to public

--- helper_functions/carousel.js
+++ helper_functions/carousel.js
@@ -1,49 +1,7 @@
 var callSendAPI = require('./callSendAPI');
 
-// function sendMovieCarousel(recipientId) {
-//   console.log('inside sendMovieCarousel');
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     message: {
-//       attachment: {
-//         type: "template",
-//         payload: {
-//           template_type: "generic",
-//           elements: [
-//             {
-//               title: "Rick and Morty",
-//               image_url: "https://ib1.hulu.com/show_key_art/22881?size=1600x600&region=US",
-//               subtitle: "Rick and Morty is a show about a sociopathic scientist who drags his unintelligent grandson on insanely dangerous adventures across the universe.",
-//               default_action: {
-//                 type: "web_url",
-//                 url: "dfdfd",
-//                 messenger_extensions: true,
-//                 webview_height_ratio: "tall"
-//               },
-//               buttons: [
-//                 {
-//                   type: "web_url",
-//                   url: "https://www.hulu.com/rick-and-morty",
-//                   title: "Watch this!"
-//                 }
-//               ]
-//             }
-//           ]
-//         }
-//       }
-//     }
-//   };
-//
-//   callSendAPI(messageData);
-// }
-
-/*
- * Send a Structured Message (Generic Message type) using the Send API.
- *
- */
 function sendMovieCarousel(recipientId) {
+  console.log('inside sendMovieCarousel');
   var messageData = {
     recipient: {
       id: recipientId
@@ -53,35 +11,22 @@ function sendMovieCarousel(recipientId) {
         type: "template",
         payload: {
           template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",
-            image_url: "https://ib1.hulu.com/show_key_art/22881?size=1600x600&region=US"",
-            buttons: [{
-              type: "web_url",
-              url: "https:www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",
-            image_url: "https:ib1.hulu.com/show_key_art/22881?size=1600x600&region=US",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble"
-            }]
-          }]
+          elements: [
+            {
+              title: "Rick and Morty",
+              image_url: "",
+              subtitle: "Rick and Morty is a show about a sociopathic scientist.",
+              item_url: "https://www.oculus.com/en-us/rift/",
+              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+              buttons: [
+                {
+                  type: "web_url",
+                  url: "https://www.hulu.com/rick-and-morty",
+                  title: "Watch this!"
+                }
+              ]
+            }
+          ]
         }
       }
     }
@@ -89,5 +34,11 @@ function sendMovieCarousel(recipientId) {
 
   callSendAPI(messageData);
 }
+
+/*
+ * Send a Structured Message (Generic Message type) using the Send API.
+ *
+ */
+
 
 module.exports = sendMovieCarousel;
