c0d81481e677ad8df44edaa99b226f855d117c2b
Sean Lee <seanboramlee@gmail.com>
2017-04-07 14:13:20 -0700

    add two more items to carousel

--- helper_functions/carousel.js
+++ helper_functions/carousel.js
@@ -13,9 +13,21 @@ function sendMovieCarousel(recipientId) {
           template_type: "generic",
           elements: [
             {
+              title: "10 Cloverfield Lane",
+              subtitle: "A young woman finds herself in an underground bunker with two men.",
+              item_url: "https://www.hulu.com/watch/1034667",
+              image_url: "https://ib1.hulu.com/show_key_art/22881?size=1600x600&region=US",
+              buttons: [
+                {
+                  type: "web_url",
+                  url: "http://pogo-movies.com/wp-content/uploads/2016/03/10-cloverfield.jpg",
+                  title: "Watch this!"
+                }
+              ]
+            },
+            {
               title: "Rick and Morty",
-              image_url: "",
-              subtitle: "Rick and Morty is a show about a sociopathic scientist.",
+              subtitle: "Rick and Morty is a show about a sociopathic scientist and his dumb grandson.",
               item_url: "https://www.hulu.com/rick-and-morty",
               image_url: "https://ib1.hulu.com/show_key_art/22881?size=1600x600&region=US",
               buttons: [
@@ -27,15 +39,14 @@ function sendMovieCarousel(recipientId) {
               ]
             },
             {
-              title: "Rick and Morty",
-              image_url: "",
-              subtitle: "Rick and Morty is a show about a sociopathic scientist.",
-              item_url: "https://www.hulu.com/rick-and-morty",
-              image_url: "https://ib1.hulu.com/show_key_art/22881?size=1600x600&region=US",
+              title: "Empire",
+              subtitle: "A drama about the head of a music empire whose family all battle for his throne.",
+              item_url: "https://www.hulu.com/empire",
+              image_url: "https://ib2.hulu.com/show_key_art/21027?size=1600x600&region=US",
               buttons: [
                 {
                   type: "web_url",
-                  url: "https://www.hulu.com/rick-and-morty",
+                  url: "https://www.hulu.com/empire",
                   title: "Watch this!"
                 }
               ]
