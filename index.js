211280326586c1380f77ba67b420382d5839d868
Sean Lee <seanboramlee@gmail.com>
2017-04-07 12:06:50 -0700

    create sendQuickEmotion file

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 
+var sendQuickEmotion = require('./sendQuickEmotion');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
@@ -57,80 +59,3 @@ function sendMessage(recipientId, message) {
         }
     });
 };
-
-
-// Quick Emotion Options
-function sendQuickEmotion(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: "Which emoji represents your current emotion?",
-      metadata: "DEVELOPER_DEFINED_METADATA",
-      quick_replies: [
-        {
-          "content_type":"text",
-          "title":"😡",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
-        },
-        {
-          "content_type":"text",
-          "title":"😂",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-        {
-          "content_type":"text",
-          "title":"😒",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
-        },
-        {
-          "content_type":"text",
-          "title":"😭",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-        {
-          "content_type":"text",
-          "title":"😊",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-      ]
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
- * Call the Send API. The message data goes in the body. If successful, we'll
- * get the message id in a response
- *
- */
-function callSendAPI(messageData) {
-  console.log("IN CALLSENDAPI !!!");
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    console.log("IN CALLSENDAPI CALLBACK!!!");
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-      console.log("IN IF BLOCK!!!")
-      if (messageId) {
-        console.log("Successfully sent message with id %s to recipient %s",
-          messageId, recipientId);
-      } else {
-      console.log("Successfully called Send API for recipient %s",
-        recipientId);
-      }
-    } else {
-      // console.log("IN ELSE BLOCK!!! ")
-      // console.log("RESPONSE IS ", response);
-      console.error(response.error);
-    }
-  });
-}
