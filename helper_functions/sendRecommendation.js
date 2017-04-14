d79d47c72182aabe72f6a4b178df6455b1608aba
Sean Lee <seanboramlee@gmail.com>
2017-04-14 09:52:20 -0700

    testing sendREcommendation

--- helper_functions/sendRecommendation.js
+++ helper_functions/sendRecommendation.js
@@ -4,36 +4,36 @@ function sendRecommendation(recipientId, userData) {
 
 
 
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [
-            {
-              title: title,
-              subtitle: subtitle,
-              item_url: item_url,
-              image_url: image_url,
-              buttons: [
-                {
-                  type: "web_url",
-                  url: item_url,
-                  title: "Watch this!"
-                }
-              ]
-            }
-          ]
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
+  // var messageData = {
+  //   recipient: {
+  //     id: recipientId
+  //   },
+  //   message: {
+  //     attachment: {
+  //       type: "template",
+  //       payload: {
+  //         template_type: "generic",
+  //         elements: [
+  //           {
+  //             title: title,
+  //             subtitle: subtitle,
+  //             item_url: item_url,
+  //             image_url: image_url,
+  //             buttons: [
+  //               {
+  //                 type: "web_url",
+  //                 url: item_url,
+  //                 title: "Watch this!"
+  //               }
+  //             ]
+  //           }
+  //         ]
+  //       }
+  //     }
+  //   }
+  // };
+  //
+  // callSendAPI(messageData);
 }
 
 
