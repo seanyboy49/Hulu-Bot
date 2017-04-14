2427abbe05aef70f792a7c0de0e0b8a2489b605a
Sean Lee <seanboramlee@gmail.com>
2017-04-14 11:53:33 -0700

    add comedy JSON object to sendRec

--- helper_functions/sendRecommendation.js
+++ helper_functions/sendRecommendation.js
@@ -6,6 +6,70 @@ function sendRecommendation(recipientId, userData) {
 var { genre, media, preference } = userData;
 console.log({genre});
 
+var comedy = {
+  movie: {
+    mainstream: [
+      {
+        title: "Daddy's Home",
+        subtitle: "Brad Whitaker is a radio host trying to get his step-children to love him.",
+        item_url: "https://www.hulu.com/watch/1010436",
+        image_url: "https://s1-ssl.dmcdn.net/PS8ls.jpg"
+      },
+      {
+        title: "Ferris Bueller’s Day Off",
+        subtitle: "A high school wise guy is determined to have an epic day off with friends.",
+        item_url: "https://www.hulu.com/watch/922958",
+        image_url: "http://www.brooklynvegan.com/files/2016/05/ferris-buellers-day-off-movie-poster-1986.jpg?w=630&h=425&zc=1&s=0&a=t&q=89"
+      }
+    ],
+    indie: [
+      {
+        title: "Everybody Wants Some!!",
+        subtitle: "A college freshman in the 80s meets his unruly new baseball teammates.",
+        item_url: "https://www.hulu.com/watch/1039730?playlist_id=2325&asset_scope=movies",
+        image_url: "http://www.impawards.com/2016/posters/everybody_wants_some_ver5.jpg"
+      },
+      {
+        title: "Hunt for the Wilderpeople",
+        subtitle: "A national manhunt is ordered for a rebellious kid and his foster uncle.",
+        item_url: "https://www.hulu.com/watch/1004156?playlist_id=2325&asset_scope=moviess",
+        image_url: "https://www.pictureshowman.com.au/cinema/wp-content/uploads/2016/06/hunt-for-the-wilderpeople-banner.jpg"
+      }
+    ]
+  },
+  tv: {
+    mainstream: [
+      {
+        title: "Brooklyn Nine-Nine ",
+        subtitle: "Detective Jake Peralta is a talented cop who doesn’t care much for rules.",
+        item_url: "https://www.hulu.com/brooklyn-nine-nine",
+        image_url: "https://images.alphacoders.com/490/thumb-1920-490603.jpg"
+      },
+      {
+        title: "The Mindy Project",
+        subtitle: "She’s a successful doctor by day, but when she leaves the office, all bets are off.",
+        item_url: "https://www.hulu.com/the-mindy-project",
+        image_url: "http://www.jillstanek.com/wp/wp-content/uploads/2015/02/The-Mindy-Project-poster.jpg"
+      }
+    ],
+    indie: [
+      {
+        title: "Fresh off the Boat",
+        subtitle: "A humorous look at immigrant life in America based on chef Eddie Huang's memoir",
+        item_url: "https://www.hulu.com/fresh-off-the-boat",
+        image_url: "https://ib1.hulu.com/show_key_art/21869?size=1600x600&region=US"
+      },
+      {
+        title: "Steven Universe",
+        subtitle: "A team of magical beings are the self-appointed guardians of the universe.",
+        item_url: "https://www.hulu.com/steven-universe",
+        image_url: "https://ib3.hulu.com/show_key_art/21721?size=1600x600&region=US"
+      }
+    ]
+  }
+}
+
+// drama object
 var drama = {
   movie: {
     mainstream: [
@@ -28,6 +92,12 @@ var drama = {
         subtitle: "Ryan enjoys living out of a suitcase until he meets a potential love interest.",
         item_url: "https://www.hulu.com/watch/1044238",
         image_url: "https://assets.mubi.com/images/notebook/post_images_danny/multiple%20images/MPOTW/up_in_the_air_detail2.jpg"
+      },
+      {
+        title: "No Country for Old Men",
+        subtitle: "Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong.",
+        item_url: "https://www.hulu.com/watch/1041536",
+        image_url: "http://img.hayhaytv.com/film/23012013/no-country-for-old-men_73131358905115.jpg"
       }
     ]
   },
@@ -52,15 +122,20 @@ var drama = {
         subtitle: "A modern reimagining of the stories that led to the Wizard of Oz",
         item_url: "https://www.hulu.com/emerald-city",
         image_url: "https://ib1.hulu.com/show_key_art/29474?size=1600x600&region=US"
+      },
+      {
+        title: "Rosewood",
+        subtitle: "A pathologist finds secrets in people's bodies using state-of-the-art equipment.",
+        item_url: "https://www.hulu.com/rosewood",
+        image_url: "https://ib1.hulu.com/show_key_art/24857?size=1600x600&region=US"
       }
     ]
   }
 }
 
 // Grab a single entry from the drama object
-var suggestion = drama[media][preference][0]
+var suggestionArray = drama[media][preference][0];
 
-console.log({media, preference});
 // Destructure vars from suggestion to be used in messageData
 var { title, subtitle, item_url, image_url } = suggestion;
 
